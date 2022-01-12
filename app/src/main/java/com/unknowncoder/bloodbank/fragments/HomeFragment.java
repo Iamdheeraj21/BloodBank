db15f6cfb30328cde7008c774d9ede514eb2644a
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-01-12 23:43:27 +0530

    set functionality of send recuest of blood in blood bank

--- app/src/main/java/com/unknowncoder/bloodbank/fragments/HomeFragment.java
+++ app/src/main/java/com/unknowncoder/bloodbank/fragments/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.unknowncoder.bloodbank.fragments;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,15 +12,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.unknowncoder.bloodbank.R;
-import com.unknowncoder.bloodbank.Send_Blood_Request;
+
+import java.util.Objects;
 
 import Notifications.Notification;
 import Notifications.NotificationAdapter;
@@ -41,11 +51,68 @@ public class HomeFragment extends Fragment {
             alertDialogBox.setMessage("1. If your information is wrong and fake then take the action against you by the Police!\n" +
                     "2. So Fill the Correct information!");
             alertDialogBox.setPositiveButton("Okay", (dialog, which) ->
-                    startActivity(new Intent(getContext(), Send_Blood_Request.class))).setNegativeButton("Cancel",null);
+                    showBloodBankRequest())
+                    .setNegativeButton("Cancel",null);
             alertDialogBox.show();
         });
         return view;
     }
+
+    private void showBloodBankRequest()
+    {
+        AlertDialog alertDialog=new AlertDialog.Builder(getContext()).create();
+        View view=LayoutInflater.from(getContext()).inflate(R.layout.activity_send_blood_request,null,false);
+        alertDialog.setView(view);
+        alertDialog.setCancelable(false);
+        alertDialog.show();
+
+        EditText name,phone_number;
+        TextView blood_group;
+        Button submit;
+
+        name=view.findViewById(R.id.editText7);
+        phone_number=view.findViewById(R.id.editText8);
+        blood_group=view.findViewById(R.id.textview4);
+        submit=view.findViewById(R.id.submit_request);
+        SharedPreferences sharedPreferences= requireContext().getSharedPreferences("MyData",MODE_PRIVATE);
+        String phonenumber=sharedPreferences.getString("mobilenumber","");
+        String name_user=sharedPreferences.getString("fullname","");
+        phone_number.setText(phonenumber);
+        name.setText(name_user);
+        submit.setOnClickListener(v->{
+            String nString=name.getText().toString().trim();
+            String phoneString=phone_number.getText().toString().trim();
+            String bloodGorup=getBloodGroup(blood_group);
+
+            if(nString.equals("")){
+                Toast.makeText(getContext(),"Enter the name...",Toast.LENGTH_SHORT).show();
+            }else if(phoneString.equals("")){
+                Toast.makeText(getContext(), "Fill the phone number", Toast.LENGTH_SHORT).show();
+            }else if(bloodGorup.equals("")){
+                Toast.makeText(getContext(), "Select the blood group..", Toast.LENGTH_SHORT).show();
+            }else{
+                 //Todo
+            }
+        });
+
+    }
+
+    private String getBloodGroup(TextView blood) {
+        final String[] blood_group = {""};
+        PopupMenu popupMenu=new PopupMenu(getContext(),blood);
+        popupMenu.getMenuInflater().inflate(R.menu.blood_groups, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                blood_group[0] = (String) menuItem.getTitle();
+                return true;
+            }
+        });
+        popupMenu.show();
+        return blood_group[0];
+    }
+
+
     private void initViews(View view){
         send_request=view.findViewById(R.id.blood_request);
         notification_recyclerView=view.findViewById(R.id.notification_recyclerView);
